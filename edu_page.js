6e1bfe772abc74bb002745196a39ac6d260aeee5
shaunakhanumante <shaunak.hanumante@gmail.com>
2026-05-12 20:02:56 -0500

    popup 2/4 + better formatting

--- edu_page.js
+++ edu_page.js
@@ -1,2 +1,5 @@
 const dialog = document.getElementById('EngPop');
 document.getElementById("Eng").onclick = () => dialog.showModal();
+
+const dialog = document.getElementById('CscPop');
+document.getElementById("Csc").onclick = () => dialog.showModal();
