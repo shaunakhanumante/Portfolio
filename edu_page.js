9e6b88524410ac467f1e275de8994cf99d0a72ba
shaunakhanumante <shaunak.hanumante@gmail.com>
2026-05-12 20:36:39 -0500

    popup 4/4

--- edu_page.js
+++ edu_page.js
@@ -3,3 +3,10 @@ document.getElementById("Eng").onclick = () => dialog.showModal();
 
 const dialog = document.getElementById('CscPop');
 document.getElementById("Csc").onclick = () => dialog.showModal();
+
+const dialog = document.getElementById('EconPop');
+document.getElementById("Econ").onclick = () => dialog.showModal();
+
+const dialog = document.getElementById('AwardsPop');
+document.getElementById("Awards").onclick = () => dialog.showModal();
+
